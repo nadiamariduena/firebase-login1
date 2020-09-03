a75027459b9856937e1fbd3568610563e905eeee
melissa <nadia.mm.mariduena@gmail.com>
2020-09-03 23:57:19 +0200

    setting up all the set states and use effect

--- src/App.js
+++ src/App.js
@@ -18,15 +18,39 @@ const App = () => {
 
   /*
 
-                                                          **   2  **
-                                       the functions connected to the STATES in step 1
+                                                          **   7  **
+                                      The following functions will serve to clean the ERRORS
+
 */
+  const clearInputs = () => {
+    // You will use the clearInputs()  function , everytime you have a user ,line:138:
+    setEmail("");
+    setPassword("");
+  };
   //
-  //---------------------
-  //  LOG IN
-  //---------------------
-  //
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+  /*                          AFTER SETTING UP the cleaning error, you will have to add them inside
+                               the functions connected to the states  in step 2 and 3
+
+
+
+
+
+
+                                                         **   2  **
+                                       the functions connected to the STATES in step 1
+                                       
+                                                      //---------------------
+                                                      //  LOG IN
+                                                      //---------------------
+                                                      //
+  */
+
   const handleLogin = () => {
+    clearErrors();
     fire // here you will start using fire library that you have imported from the fire.js
       .auth() //auth stands for Authentification
       .signInWithEmailAndPassword(email, password)
@@ -44,12 +68,21 @@ const App = () => {
         }
       });
   };
-  //
-  //---------------------
-  //  SIGN UP
-  //---------------------
-  //
+
+  /*
+
+
+                                                          **   3  **
+                                                      
+                                                    //---------------------
+                                                    //  SIGN UP
+                                                    //---------------------
+                                                    //
+                                                        
+*/
+
   const handleSignup = () => {
+    clearErrors;
     fire // here you will start using fire library that you have imported from the fire.js
       .auth() //auth stands for Authentification
       .createUserWithEmailAndPassword(email, password)
@@ -66,16 +99,76 @@ const App = () => {
         }
       });
   };
-  //
-  //---------------------
-  //  LOG OUT
-  //---------------------
-  //
   /*
 
 
+                                                          **   4  **
+   
+                                                        
+                                                    //---------------------
+                                                    //  LOG OUT
+                                                    //---------------------
+                                                    //                                               
 
 */
+
+  const handleLogout = () => {
+    fire.auth().signOut();
+  };
+  /*
+  
+
+                                                          **   5  **
+ 
+    
+                                                    //
+                                                    //---------------------
+                                                    //  LISTENER to see if user exists
+                                                    //---------------------
+                                                    //                                                      
+
+                                          // after the function log out, we are going to NEED  
+                                          listener to see if USER exist
+*/
+
+  const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        // if there is a User you want to SETUSER to that user(line:11)
+        setUser(user);
+      } else {
+        // here you want also to SETUSER but you will give an empty string if there s not USER, like so  setUser("");
+        setUser("");
+      }
+    });
+  };
+  /*
+
+                                                          **   6  **
+                                  If you remember, you also requiŕed the use EFFECT in line (1)
+
+
+                                                  
+                                                      //---------------------
+                                                      //  USE EFFECT
+                                                      //---------------------
+    
+    
+    
+    
+    
+                                                   
+*/
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  //                                --------------------------------------------------------------
+  //
+  //                                --------------------------------------------------------------
+  //
   return (
     <React.Fragment>
       <div id="page">
