fa9f2b85d93e787eac42a5bcfdeeeb5a51c41c48
melissa <nadia.mm.mariduena@gmail.com>
2020-09-05 00:19:34 +0200

    styles hero page

---
+++ build/precache-manifest.dfc79c27cd16e01a083323246021aa2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed510c5dcc97593263f037c0b10765d",
+    "url": "/firebase-login1/index.html"
+  },
+  {
+    "revision": "12957d3330807939252f",
+    "url": "/firebase-login1/static/css/main.fa061e65.chunk.css"
+  },
+  {
+    "revision": "a127a686fcc560c3f770",
+    "url": "/firebase-login1/static/js/2.93a1b871.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/firebase-login1/static/js/2.93a1b871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12957d3330807939252f",
+    "url": "/firebase-login1/static/js/main.8fc61528.chunk.js"
+  },
+  {
+    "revision": "e2fd6494c22c550a7b1d",
+    "url": "/firebase-login1/static/js/runtime-main.b7b063e7.js"
+  },
+  {
+    "revision": "67f4cc60c1f814b4f464e5ac19ada164",
+    "url": "/firebase-login1/static/media/AnnaRosova_metalmagazine-20.67f4cc60.jpg"
+  }
+]);
--- src/comps/hero.js
+++ src/comps/hero.js
@@ -4,10 +4,26 @@ const Hero = ({ handleLogout }) => {
   // the hero props destructured
   return (
     <section className="hero">
-      <nav>
-        <h1>hello hero</h1>
-        <button onClick={handleLogout}>LogOut</button>
+      <nav className="navbar">
+        <h1>WELCOME</h1>
+        <button onClick={handleLogout}>Exit</button>
       </nav>
+
+      <div className="top-hero">
+        <div className="video-container">
+          {/* <p>find a way to add a never ending scroll in react</p> */}
+          <div className="video">
+            <iframe
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/73nw0yKjyQE"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
